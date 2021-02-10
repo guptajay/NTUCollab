998451091fe25eaa7db78edafa44a5867ea5ce00
Jay Gupta <jay002@e.ntu.edu.sg>
2021-02-11 00:57:34 +0800

    another test bug fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,5 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ntucollab/main.dart';
 
 void main() {
-
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    expect(1, 1);
+  });
 }
